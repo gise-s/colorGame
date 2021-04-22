c5d71a307ba98efb004ac959a68bc7dceaa8e5a7
Gise <73615762+gise-s@users.noreply.github.com>
2021-04-21 22:59:08 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,39 +1,135 @@
-let boton = document.querySelector("button");
-let ingreso = document.querySelector("input");
-
-function cargarCiudad(ciudad) {
-
-    $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + encodeURI(ciudad) + "&appid=95176c8edea30e33338e0eaddd53a916&units=metric&lang=es", function (data) {
-
-        document.querySelector("#temperatura").textContent = `${Math.floor(data.main.temp)}°C`;
-        document.querySelector("#ciudad").textContent = ciudad.toUpperCase();
-        document.querySelector("img").setAttribute("src",
-            `http://openweathermap.org/img/w/${data.weather[0].icon}.png`)
-        document.querySelector("#descripcion").textContent = data.weather[0].description
-        document.querySelector(".container").style.visibility = "visible";
-        ingreso.value = ""
-    }).fail(function() {
-        if(ingreso.value.length>0) {
-            document.querySelector(".container").style.visibility = "hidden";
-             alert("Ciudad no encontrada");
-         } else {
-            document.querySelector(".container").style.visibility = "hidden";
-             alert("Ingrese una ciudad, por favor")
-         }
-        })
+//***variables***//
+let cuadrados = document.querySelectorAll(".square");
+let number = 6;
+let colors = generateRandomColors(number);
+let colorDisplay = document.querySelector("#colorDisplay")
+let pickedColor = pickColor(6)
+colorDisplay.textContent=pickedColor
+let bReset = document.querySelector("#reset");
+let easy = document.querySelector("#easy");
+let hard = document.querySelector("#hard");
+let h1 = document.querySelector("h1")
+let clickedColor
+////****////
+function pickColor(num) {
+   let i = Math.floor(Math.random() * num)
+return colors[i] 
 };
 
-boton.addEventListener("click", function () {
-    let ciudad = ingreso.value;
-    cargarCiudad(ciudad);
-   
-    
-});
+//itera sobre los cuadrados para agregarles el color
 
-ingreso.addEventListener("keypress", function (e) {   
-    if (e.key === 'Enter') {
-    let ciudad = ingreso.value;
-    cargarCiudad(ciudad);
-    }
-});
+for (let i = 0; i < cuadrados.length; i++) {
+   cuadrados[i].style.backgroundColor = colors[i]
+   //agrego el evento click y comparo si es = el color elegido con el de pickColor
+   cuadrados[i].addEventListener("click", function () {
+      let clickedColor = this.style.backgroundColor
+      if (clickedColor == colorDisplay.textContent) {
+         message.textContent = "CORRECT!"
+         h1.style.backgroundColor = clickedColor
+         bReset.textContent = "Play again?"
+         changeColor()
+      }
+      else {
+         this.style.backgroundColor = " #232323"
+         message.textContent = "TRY AGAIN"
+      }
+   });
+};
+//cambia de color los cuadrados al del titulo
+function changeColor(i) {
+   for (let i = 0; i < cuadrados.length; i++) {
+      cuadrados[i].style.backgroundColor = document.querySelector("#colorDisplay").textContent
+   }
+};
 
+
+
+
+//generateRandomColors, que creará de forma random nuestro arreglo de colores utilizando la función randomColor que acabamos de crear
+
+function generateRandomColors(num) {
+   let color = []
+   for (let i = 0; i < num; i++) {
+      color[i] = randomColor()
+   }
+   return color
+};
+//Crea la función randomColor para generar y devolver un color random(recuerda que rgb son siempre 3 números entre 0 y 255).
+
+function randomColor() {
+   let a = Math.floor(Math.random() * 256)
+   let b = Math.floor(Math.random() * 256)
+   let c = Math.floor(Math.random() * 256)
+   return (`rgb(${a}, ${b}, ${c})`)
+};
+
+
+
+
+// boton RESET
+
+function reset() {
+   bReset.addEventListener("click", function () {
+      colors = generateRandomColors(number)
+      pickedColor = pickColor(6)
+      for (let i = 0; i < cuadrados.length; i++) {
+         cuadrados[i].style.backgroundColor = colors[i]
+      }
+      bReset.textContent= "New color";
+     message.textContent = ""
+      h1.style.background = "#232323"
+      document.querySelector("#colorDisplay").textContent = pickedColor;
+   })
+}
+
+//genero una función que asigne el color del título
+//Ahora vamos a crear una función para asignarle a pickedColor un color random de nuestro arreglo de colores. 
+//Para eso vamos a crear la función pickColor que devuelva un color random:
+
+
+
+
+
+//boton easy y hard
+
+function botoms() {
+
+   easy.addEventListener("click", function () {
+      easy.classList.add("selected")
+      hard.classList.remove("selected")
+      number = 3
+      colors = generateRandomColors(number)
+      pickedColor = pickColor(3)
+      message.textContent = ""
+      bReset.textContent= "New color"
+      h1.style.backgroundColor="#232323"
+      document.querySelector("#colorDisplay").textContent = pickedColor;
+      for (let i = 0; i < cuadrados.length; i++) {
+         if (cuadrados[i] = colors[i]) {
+            cuadrados[i].style.backgroundColor = colors[i]
+         } else {
+            cuadrados[i].style.display = "none"
+         }
+      }
+   }
+   )
+
+
+   hard.addEventListener("click", function () {
+      hard.classList.add("selected")
+      easy.classList.remove("selected")
+      number = 6
+      colors = generateRandomColors(number)
+      pickedColor = pickColor(6)
+      message.textContent = ""
+      bReset.textContent= "New color"
+      h1.style.backgroundColor="#232323"
+      document.querySelector("#colorDisplay").textContent = pickedColor
+      for (let i = 0; i < cuadrados.length; i++) {
+         cuadrados[i].style.backgroundColor = colors[i]
+         cuadrados[i].style.display = "block"
+      }
+   })
+}
+botoms()
+reset()
